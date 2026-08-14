49c0384b3e609bfa05420aae0a639e71ce0dadcb
gasharva <vorikazdinoel@gmail.com>
2026-08-14 21:17:29 +0300

    Render both measure and primitive classification diagnostics

--- Experiments/SvgStructure/Program.cs
+++ Experiments/SvgStructure/Program.cs
@@ -7,17 +7,20 @@ if (args.Length == 0)
 }
 
 var svgPath = args[0];
-var outputPath = args.Length > 1 ? args[1] : null;
+var classifiedOutputPath = args.Length > 1 ? args[1] : null;
 
 var geometryReader = new SvgSceneGeometryReader();
 var systemDetector = new StaffSystemDetector();
 var structureBuilder = new ScoreStructureBuilder();
-var overlayRenderer = new MeasureOverlayRenderer();
+var measureOverlayRenderer = new MeasureOverlayRenderer();
+var classificationRenderer = new PrimitiveClassificationRenderer();
 
 var lines = geometryReader.ReadLines(svgPath);
 var systems = systemDetector.Detect(lines);
 var score = structureBuilder.Build(systems);
-var classifiedImagePath = overlayRenderer.Render(svgPath, systems, outputPath);
+
+var measureImagePath = measureOverlayRenderer.Render(svgPath, systems);
+var classifiedImagePath = classificationRenderer.Render(svgPath, systems, classifiedOutputPath);
 
 Console.WriteLine($"lines: {lines.Count}");
 Console.WriteLine($"systems: {systems.Count}");
@@ -31,4 +34,5 @@ foreach (var part in score.Parts)
 }
 
 Console.WriteLine();
-Console.WriteLine($"classified overlay: {classifiedImagePath}");
+Console.WriteLine($"measure overlay:      {measureImagePath}");
+Console.WriteLine($"classified primitives: {classifiedImagePath}");
