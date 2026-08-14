498dcf38e5cc45b54f5e5aeada399640eb69736d
gasharva <vorikazdinoel@gmail.com>
2026-08-14 15:52:02 +0300

    Add configurable symbol recognition parallelism

--- Configuration/RecognitionConfig.cs
+++ Configuration/RecognitionConfig.cs
@@ -11,4 +11,5 @@ public sealed class RecognitionConfig
     public double MaxSymbolDistanceInSpaces { get; init; } = 5.0;
     public double MaxAttachmentDistanceInSpaces { get; init; } = 2.5;
     public double MinClassificationScore { get; init; } = 0.42;
+    public int RecognitionParallelism { get; init; } = 8;
 }
