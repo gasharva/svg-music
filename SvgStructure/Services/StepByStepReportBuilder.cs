42186a1ec0c69f909e815ee603813d4b3be4ba73
gasharva <vorikazdinoel@gmail.com>
2026-08-16 17:03:35 +0300

    Link primitive SVG galleries from step-by-step report

--- SvgStructure/Services/StepByStepReportBuilder.cs
+++ SvgStructure/Services/StepByStepReportBuilder.cs
@@ -28,11 +28,11 @@ public sealed class StepByStepReportBuilder
             }
 
             html.Append($"<td><a href=\"{dir}/measures.png\"><img class=\"preview\" src=\"{dir}/measures.png\"></a></td>");
-            html.Append($"<td><a href=\"{dir}/classified.png\"><img class=\"preview\" src=\"{dir}/classified.png\"></a></td>");
+            html.Append($"<td><a href=\"{dir}/classified.png\"><img class=\"preview\" src=\"{dir}/classified.png\"></a><br><a href=\"{dir}/primitives.html\"><b>primitive SVG gallery ({item.ExportedPrimitiveCount})</b></a></td>");
             html.Append($"<td><a href=\"{dir}/meters.png\"><img class=\"preview\" src=\"{dir}/meters.png\"></a><br><b>meters: {item.MeterCount}</b><br><b>clefs: {item.ClefCount}</b><br><a href=\"{dir}/meter-inputs/index.html\"><b>meter inputs</b></a><br><a href=\"{dir}/clef-inputs/README.md\"><b>clef inputs</b></a></td>");
             html.Append("<td class=\"mono\">" +
                         $"lines: {item.LineCount}<br>systems: {item.SystemCount}<br>parts: {item.PartCount}<br>measures: {item.MeasureCount}<br>" +
-                        $"P+M primitives: {item.PartMeasurePrimitiveCount}<br>M-only: {item.MeasurePrimitiveCount}<br>physical-only: {item.PhysicalOnlyPrimitiveCount}<br>meters: {item.MeterCount}<br>clefs: {item.ClefCount}<br>" +
+                        $"P+M primitives: {item.PartMeasurePrimitiveCount}<br>exported SVGs: {item.ExportedPrimitiveCount}<br>M-only: {item.MeasurePrimitiveCount}<br>physical-only: {item.PhysicalOnlyPrimitiveCount}<br>meters: {item.MeterCount}<br>clefs: {item.ClefCount}<br>" +
                         $"<a href=\"{dir}/structure.json\">structure.json</a></td>");
             html.AppendLine("</tr>");
         }
@@ -63,9 +63,9 @@ public sealed class StepByStepReportBuilder
             md.AppendLine(
                 $"| **[{item.FileName}]({dir}/source.svg)** | " +
                 $"[![blocks]({dir}/measures.png)]({dir}/measures.png) | " +
-                $"[![primitives]({dir}/classified.png)]({dir}/classified.png) | " +
+                $"[![primitives]({dir}/classified.png)]({dir}/classified.png)<br>[primitive SVG gallery]({dir}/primitives.html) ({item.ExportedPrimitiveCount}) | " +
                 $"[![symbols]({dir}/meters.png)]({dir}/meters.png)<br>meters={item.MeterCount}<br>clefs={item.ClefCount}<br>[meter inputs]({dir}/meter-inputs/index.html)<br>[clef inputs]({dir}/clef-inputs/README.md) | " +
-                $"parts={item.PartCount}<br>measures={item.MeasureCount}<br>P+M={item.PartMeasurePrimitiveCount}<br>M-only={item.MeasurePrimitiveCount}<br>physical={item.PhysicalOnlyPrimitiveCount}<br>[json]({dir}/structure.json) |");
+                $"parts={item.PartCount}<br>measures={item.MeasureCount}<br>P+M={item.PartMeasurePrimitiveCount}<br>exported={item.ExportedPrimitiveCount}<br>M-only={item.MeasurePrimitiveCount}<br>physical={item.PhysicalOnlyPrimitiveCount}<br>[json]({dir}/structure.json) |");
         }
 
         md.AppendLine();
