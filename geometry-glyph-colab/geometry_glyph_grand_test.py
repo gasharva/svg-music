a069a913b6335fc34268e5be830cfc8310a87c92
gasharva <vorikazdinoel@gmail.com>
2026-08-25 18:04:54 +0300

    Fix k-NN classifier label lookup

--- geometry-glyph-colab/geometry_glyph_grand_test.py
+++ geometry-glyph-colab/geometry_glyph_grand_test.py
@@ -28,6 +28,7 @@ CLASSIFIERS = [
     ('5-NN weighted', '5nnw'),
     ('7-NN weighted', '7nnw'),
 ]
+CLASSIFIER_LABELS = {key: label for label, key in CLASSIFIERS}
 
 
 def _svg_data_uri(svg_bytes):
@@ -220,7 +221,7 @@ def launch_grand_test(viewer, point_count=16, count_penalty=0.35, top_k=5):
             topk_text=' · '.join(f'{classes[j]}/{PurePosixPath(files[j]).stem} ({distances[i,j]:.4f})' for j in order[:show_k])
             rows.append({'index':i,'class':classes[i],'font':fonts[i],'nearest_correct':nc,'nearest_wrong':nw,'nearest_margin':nm,'farthest_correct':fc,'class_margin':cm,'classifier_ok':ok,'predicted':pred,'topk_text':topk_text})
         state['rows']=rows
-        label=dict(CLASSIFIERS)[mode]
+        label=CLASSIFIER_LABELS[mode]
         with summary_output:
             clear_output(wait=True)
             display(HTML(f'<b>{html.escape(label)}</b>: {correct}/{n} = {correct/n:.1%}'))
@@ -230,7 +231,7 @@ def launch_grand_test(viewer, point_count=16, count_penalty=0.35, top_k=5):
         if state['rows'] is None: return
         with table_output:
             clear_output(wait=True)
-            display(HTML(_build_table(state['rows'],state['svg_bytes_cache'],sort_widget.value,direction_widget.value,dict(CLASSIFIERS)[classifier_widget.value])))
+            display(HTML(_build_table(state['rows'],state['svg_bytes_cache'],sort_widget.value,direction_widget.value,CLASSIFIER_LABELS[classifier_widget.value])))
 
     def run_geometry(_=None):
         run_button.disabled=True
